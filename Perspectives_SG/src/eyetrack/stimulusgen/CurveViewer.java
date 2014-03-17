dd68a480f2ab4bf2ee45441d5211983663099b59
nahid204 <nferd001@fiu.edu>
2014-03-17 01:24:42 -0400

    #7
    Color changes of curve viewer is implemented

--- Perspectives_SG/src/eyetrack/stimulusgen/CurveViewer.java
+++ Perspectives_SG/src/eyetrack/stimulusgen/CurveViewer.java
@@ -1,19 +1,30 @@
 package eyetrack.stimulusgen;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.util.Random;
 
+import perspectives.DefaultProperties.ColorPropertyType;
+import perspectives.DefaultProperties.DoublePropertyType;
 import perspectives.Property;
 import perspectives.Viewer2D;
 import perspectives.DefaultProperties.IntegerPropertyType;
 import util.SplineFactory;
+import util.Util;
 
 public class CurveViewer extends Viewer2D{
 	public static final String PROPERTY_NAME_MIN_DISTANCE = "Distance.Minimum Distance";
 	public static final String PROPERTY_NAME_MAX_DISTANCE = "Distance.Maximum Distance";
 	public static final String PROPERTY_NAME_OBJECT_COUNT = "Object Count";
+	
+	public static final String PROPERTY_NAME_COLOR1 = "Color1";
+	public static final String PROPERTY_NAME_COLOR2 = "Color2";
+	public static final String PROPERTY_NAME_SEGMENT1 = "Segment1";
+	public static final String PROPERTY_NAME_SEGMENT2 = "Segment2";
+	
+	
 
 	private int []curveX;
 	private int []curveY;
@@ -38,6 +49,22 @@ public class CurveViewer extends Viewer2D{
 			Property<IntegerPropertyType> objectCount = new Property<IntegerPropertyType>(PROPERTY_NAME_OBJECT_COUNT);
 			objectCount.setValue(new IntegerPropertyType(15));
 			this.addProperty(objectCount);
+			
+			Property<ColorPropertyType> color1 = new Property<ColorPropertyType>(PROPERTY_NAME_COLOR1);
+			color1.setValue(new ColorPropertyType(Color.blue));
+			this.addProperty(color1);
+			
+			Property<ColorPropertyType> color2 = new Property<ColorPropertyType>(PROPERTY_NAME_COLOR2);
+			color2.setValue(new ColorPropertyType(Color.gray));
+			this.addProperty(color2);
+			
+			Property<DoublePropertyType> segment1 = new Property<DoublePropertyType>(PROPERTY_NAME_SEGMENT1);
+			segment1.setValue(new DoublePropertyType(0.5));
+			this.addProperty(segment1);
+			
+			Property<DoublePropertyType> segment2 = new Property<DoublePropertyType>(PROPERTY_NAME_SEGMENT2);
+			segment2.setValue(new DoublePropertyType(0.8));
+			this.addProperty(segment2);
 		}
 		catch (Exception e) {		
 			e.printStackTrace();
@@ -47,6 +74,18 @@ public class CurveViewer extends Viewer2D{
 	{
 		Property<IntegerPropertyType> prop = this.getProperty(name);
 		return prop.getValue().intValue();
+	}
+	
+	private double getPropertyDoubleValue(String name)
+	{
+		Property<DoublePropertyType> prop = this.getProperty(name);
+		return prop.getValue().doubleValue();
+	}
+	
+	private Color getPropertyColorValue(String name)
+	{
+		Property<ColorPropertyType> prop = this.getProperty(name);
+		return prop.getValue().colorValue();
 	}
 	
 	private void positionLayout()
@@ -104,22 +143,105 @@ public class CurveViewer extends Viewer2D{
 	public <T extends perspectives.PropertyType> void propertyUpdated(Property p, T newvalue)
 	{
 		positionLayout();
+		if(p.getName()== PROPERTY_NAME_SEGMENT1)
+		{
+			
+			double newValue = ((DoublePropertyType) newvalue).doubleValue();
+			double segment2Val = this.getPropertyDoubleValue(PROPERTY_NAME_SEGMENT2);
+			if(newValue < 0)
+			{
+				DoublePropertyType val = new DoublePropertyType(0.0);
+				((Property<DoublePropertyType>) p).setValue(val);
+			}
+			else if(newValue > 1)
+			{
+				DoublePropertyType val = new DoublePropertyType(1.0);
+				((Property<DoublePropertyType>) p).setValue(val);
+			}
+			else if(newValue >segment2Val)
+			{
+				DoublePropertyType val = new DoublePropertyType(0.0);
+				((Property<DoublePropertyType>) p).setValue(val);
+			}
+			
+			
+		}
+		else if(p.getName()== PROPERTY_NAME_SEGMENT2)
+		{
+			double newValue = ((DoublePropertyType) newvalue).doubleValue();
+			double segment1Val = this.getPropertyDoubleValue(PROPERTY_NAME_SEGMENT1);
+			if(newValue < 0)
+			{
+				DoublePropertyType val = new DoublePropertyType(0.0);
+				((Property<DoublePropertyType>) p).setValue(val);
+			}
+			else if(newValue > 1)
+			{
+				DoublePropertyType val = new DoublePropertyType(1.0);
+				((Property<DoublePropertyType>) p).setValue(val);
+			}
+			else if(newValue < segment1Val)
+			{
+				DoublePropertyType val = new DoublePropertyType(1.0);
+				((Property<DoublePropertyType>) p).setValue(val);
+			}
+		}
 	}
+	
+	
 	@Override
 	public void render(Graphics2D g) {
 		// TODO Auto-generated method stub
 		
-		g.setColor(Color.blue);
+		
 		if(this.curveX != null && this.curveY != null)
 		{
 			Point p1 = new Point(this.curveX[0], this.curveY[0]);
 			Point p2 = null;
+			g.setColor(Color.red);
+			g.fillOval((p1.x)-5, (p1.y)-5, 10, 10);
+			Font font = new Font("Verdana", Font.BOLD, 16);
+			g.setFont(font);
+			g.drawString("A", (p1.x)-10, (p1.y)-10);
+			Color color1 = this.getPropertyColorValue(PROPERTY_NAME_COLOR1);
+			Color color2 = this.getPropertyColorValue(PROPERTY_NAME_COLOR2);
+
+			double segment1 = this.getPropertyDoubleValue(PROPERTY_NAME_SEGMENT1);
+			double segment2 = this.getPropertyDoubleValue(PROPERTY_NAME_SEGMENT2);
+			
+			int firstSegment = (int) ( this.curveX.length * segment1);
+			int secondSegment = (int) ( this.curveX.length * segment2);
+			
 			for(int i=1;i<this.curveX.length;i++)
 			{
+				if(i< firstSegment)
+				{
+					g.setColor(color1);
+				}
+				else if(i> secondSegment)
+				{
+					g.setColor(color2);
+				}
+				else
+				{
+					int m = i-firstSegment;
+					int n= secondSegment-i;
+
+					int r = (n * color1.getRed()+ m*color2.getRed())/(m+n);
+					int gr = (n * color1.getGreen()+ m*color2.getGreen())/(m+n);
+					int b = (n * color1.getBlue()+ m*color2.getBlue())/(m+n);
+					Color color3= new Color(r,gr,b);
+					g.setColor(color3);
+					
+				}
 				p2 = new Point(this.curveX[i],this.curveY[i]);
 				g.drawLine(p1.x, p1.y, p2.x, p2.y);
 				p1 = p2;
 			}
+			g.setColor(Color.red);
+			g.fillOval((p2.x)-5, (p2.y)-5, 10, 10);
+			g.setFont(font);
+			g.drawString("B", (p2.x)+10, (p2.y)-10);
 		}
 		
 	}
